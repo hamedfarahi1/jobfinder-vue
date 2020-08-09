8708ac1fd7a6f46171333f8e99ac817cd7ade31d
hamed <hamedfarahikharazmi@gmail.com>
2020-08-10 02:44:59 +0430

    + yess

--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
 import Vue from "vue";
-import Main from "./components/Main.vue"
-import VueRouter from 'vue-router'
+import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-Vue.use(VueRouter)
 
-const routes = [
-  { path: '/home', component: Main },
-]
-
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
-const router = new VueRouter({
-  routes // short for `routes: routes`
-})
-
-const app = new Vue({
-  router
+new Vue({
+  render: h => h(App)
 }).$mount("#app");
-console.log(app)
